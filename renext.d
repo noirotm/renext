eb9c00f42eb6a19a6d1f67e9fe1824b83720a7e3
Marc Noirot <marc.noirot@gmail.com>
2014-01-27 12:50:56 +0100

    Refactored command line parsing.

--- renext.d
+++ renext.d
@@ -1,8 +1,9 @@
 import std.stdio;
-import std.c.stdlib;
 import std.conv;
 import std.file;
-import cmdline;
+import std.getopt;
+import std.c.process;
+
 import rename;
 
 const char[] RENEXT_VERSION = "1.0.5";
@@ -24,80 +25,59 @@ void usage() {
              "  -i, --ignore-case ignore case in findRE\n"
              "  -r, --recursive   search subdirectories recursively\n"
              "  -t, --test        don't change anything, just print possible changes\n"
-             "      --verbose=x   verbose (x=0: no output, x=1: default output, x=2: much output)\n"
+             "  -v, --verbose     verbosity level (default: no output, -v: default output, -vv: much output)\n"
              "  -q, --quiet       same as verbose=0\n"
              "  -V, --version     display version and exit.\n"
              "  -h, --help        display this help and exit.\n"
              "\n"
-             "examples: renext \"ASD\" \"asd\"\n"
-             "\n"
-             "          renext -r \"(\\d)(\\d)\" \"$2$1\"");
+             "examples:\n"
+             "  renext \"ASD\" \"asd\"\n"
+             "  renext -r \"(\\d)(\\d)\" \"$2$1\"");
 }
 
 void printVersion() {
-    writefln("Rename Extended version %s - (c) 2006, Marc Noirot", RENEXT_VERSION);
+    writefln("Rename Extended version %s - (c) 2006-2014, Marc Noirot", RENEXT_VERSION);
 }
 
-int main(char [][] args) {
+int main(string[] args) {
+    auto renamer = new Renamer;
+    
+    // parse command line
+    try {
+	    getopt(args,
+	    	std.getopt.config.caseSensitive,
+	    	std.getopt.config.bundling,
+	    	"capitalize|c",		&renamer.capitalize,
+	    	"rename-all|a", 	{ renamer.renameDirs = true; renamer.renameFiles = true; },
+	    	"rename-dirs|d", 	{ renamer.renameDirs = true; renamer.renameFiles = false; },
+	    	"global|g", 		&renamer.global,
+	    	"ignore-case|i", 	&renamer.ignoreCase,
+	    	"recursive|r",		&renamer.recursive,
+	    	"test|t",			&renamer.test,
+	    	"verbose+|v+",		&renamer.verbosity,
+	    	"quiet|q",			{ renamer.verbosity = 0; },
+	    	"version|V",		{ printVersion(); exit(1); },
+	    	"help|h",			{ usage(); exit(1); }
+	    );
+    }
+    catch (Exception e) {
+    	stderr.writefln(e.msg);
+    	usage();
+    	return 1;
+    }
+
     if (args.length < 2) {
         usage();
+        return 1;
     }
-    else {
-        auto renamer = new Renamer;
-        auto p = new CmdlineParser;
+    
+    renamer.findRe = args[1];
+    renamer.replaceRe = args[2];
 
-        p.addOption("capitalize", 'c',  { renamer.capitalize = true; });
-        p.addOption("rename-all", 'a',  { renamer.renameDirs = true; renamer.renameFiles = true; });
-        p.addOption("rename-dirs", 'd', { renamer.renameDirs = true; renamer.renameFiles = false; });
-        p.addOption("global", 'g',      { renamer.global = true; });
-        p.addOption("ignore-case", 'i', { renamer.ignoreCase = true; });
-        p.addOption("recursive", 'r',   { renamer.recursive = true; });
-        p.addOption("test", 't',        { renamer.test = true; });
-        p.addOption("verbose", '\0', {
-            if (p.option.length == 0) {
-                stderr.writefln("No argument provided for option %s.", p.option);
-                exit(EXIT_FAILURE);
-            }
-            else {
-                try {
-                    int level = to!int(p.arg);
-                    if (level < 0 || level > 2)
-                        throw new ConvException("");
-                    renamer.verbosity = level;
-                }
-                catch (ConvException e) {
-                    stderr.writefln("Invalid verbosity level: %s", p.arg);
-                        exit(EXIT_FAILURE);
-                }
-            }
-        });
-        p.addOption("quiet", 'q',       { renamer.verbosity = 0; });
-        p.addOption("version", 'V',     { printVersion(); exit(EXIT_SUCCESS); });
-        p.addOption("help", 'h',        { usage(); exit(EXIT_SUCCESS); });
-        p.setDefaultAction({
-            if (p.index == p.argc-2) {
-                renamer.findRe = p.option;
-            }
-            else if (p.index == p.argc-1) {
-                renamer.replaceRe = p.option;
-            }
-            else if (p.index != 0) {
-                stderr.writefln("Syntax error: %s", p.option);
-                exit(EXIT_FAILURE);
-            }
-        });
-        p.setUnknownOptionAction({
-            stderr.writefln("Unknown option: %s\n", p.option);
-            usage();
-            exit(EXIT_FAILURE);
-        });
-
-        p.parse(args);
-
-        uint renamed = renamer.renameInDir(getcwd());
-        if (!renamer.test) {
-            writefln("\n%d files successfully renamed.", renamed);
-        }
+    uint renamed = renamer.renameInDir(getcwd());
+    if (!renamer.test) {
+        writefln("\n%d files successfully renamed.", renamed);
     }
+
     return 0;
 }
