a9db7ef93346821d05c3a7d3208f1b41e8172ddb
Marc Noirot <marc.noirot@gmail.com>
2014-01-27 10:42:10 +0100

    Fixed compilation with modern D language compilers.

--- rename.d
+++ rename.d
@@ -4,8 +4,8 @@ import std.stdio;
 import std.file;
 import std.path;
 import std.string;
-import std.regexp;
-import std.ctype;
+import std.regex;
+import std.ascii;
 
 /*
     File and directories renaming class
@@ -38,19 +38,19 @@ class Renamer {
         if (global)
             attrib ~= "g";
 
-        auto re = new RegExp(findRe, attrib);
+        auto re = regex(findRe, attrib);
 
-        auto entries = std.file.listdir(dir);
-        foreach (e; entries) {
-            bool isDir = (isdir(std.path.join(dir, e)) != 0);
+        auto entries = dirEntries(dir, SpanMode.shallow);
+        foreach (string e; entries) {
+            bool isDir = (isDir(std.path.buildPath(dir, e)) != 0);
             if (isDir)
                 directories ~= e;
 
-            if (((renameDirs && isDir) || (renameFiles && !isDir)) && re.test(e)) {
+            if (((renameDirs && isDir) || (renameFiles && !isDir)) && match(e, re)) {
                 if (replacements.length == 0 && verbosity == 1)
                     writefln("\n  %s", dir);
 
-                auto repl = re.replace(e, replaceRe);
+                auto repl = replace(e, re, replaceRe);
                 replacements[e] = repl;
                 if (capitalize)
                     repl = capstr(repl);
@@ -71,15 +71,15 @@ class Renamer {
 
         if (recursive) {
             foreach (d; directories) {
-                if (cmp(d, curdir) || cmp(d, pardir)) {
-                    renamed += renameInDir(std.path.join(dir, d), namesCallback);
+                if (d != "." || d != "..") {
+                    renamed += renameInDir(std.path.buildPath(dir, d), namesCallback);
                 }
             }
         }
 
         if (!test) {
             foreach (e, repl; replacements) {
-                std.file.rename(std.path.join(dir, e), std.path.join(dir, repl));
+                std.file.rename(std.path.buildPath(dir, e), std.path.buildPath(dir, repl));
                 ++renamed;
             }
         }
@@ -91,13 +91,13 @@ class Renamer {
         bool isBoundary = true;
 		char[] result;
         foreach (c; s) {
-            if (isspace(c)) {
+            if (std.ascii.isWhite(c)) {
                 isBoundary = true;
 				result ~= c;
 			}
             else if (isBoundary) {
                 isBoundary = false;
-                result ~= std.ctype.toupper(c);
+                result ~= std.ascii.toUpper(c);
             }
         }
 		return result.idup;
